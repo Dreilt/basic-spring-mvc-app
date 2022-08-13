71614379c1cfb05e168d188ef483804b03958bd9
Dreilt <patrykdreilt@gmail.com>
2022-08-13 23:06:48 +0200

    Create custom login form

---
+++ src/main/java/pl/dreilt/basicspringmvcapp/configuration/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package pl.dreilt.basicspringmvcapp.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requests -> requests
+                .anyRequest().authenticated());
+        http.formLogin(login -> login.loginPage("/login").permitAll());
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().mvcMatchers(
+                "/styles/**"
+        );
+    }
+}
---
+++ src/main/java/pl/dreilt/basicspringmvcapp/controller/LoginController.java
@@ -0,0 +1,13 @@
+package pl.dreilt.basicspringmvcapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String getLoginForm() {
+        return "forms/login-form";
+    }
+}
