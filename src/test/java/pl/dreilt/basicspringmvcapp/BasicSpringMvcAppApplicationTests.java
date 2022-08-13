efb2a5ed02a7257e0f932a29c2b839a6f4e16eed
Dreilt <patrykdreilt@gmail.com>
2022-08-13 21:26:37 +0200

    App generated by Spring Initializr

---
+++ src/test/java/pl/dreilt/basicspringmvcapp/BasicSpringMvcAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dreilt.basicspringmvcapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringMvcAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
