1d45da6d89c62e160ef7efeb22747bff5ed93974
Dreilt <patrykdreilt@gmail.com>
2022-08-25 21:44:47 +0200

    Add server side validation to app user edit profile form

--- src/main/java/pl/dreilt/basicspringmvcapp/controller/AppUserController.java
+++ src/main/java/pl/dreilt/basicspringmvcapp/controller/AppUserController.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -46,12 +47,16 @@ public class AppUserController {
     }
 
     @PatchMapping("/settings/profile")
-    public String updateAppUserProfile(@Valid @ModelAttribute AppUserEditProfileDto appUserEditProfileDto) {
-        Optional<AppUserEditProfileDto> appUserProfileUpdated = appUserService.updateAppUserProfile(appUserEditProfileDto);
-        if (appUserProfileUpdated.isPresent()) {
-            return "redirect:/settings/profile";
+    public String updateAppUserProfile(@Valid @ModelAttribute AppUserEditProfileDto appUserEditProfileDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "forms/app-user-edit-profile-form";
         } else {
-            return null;
+            Optional<AppUserEditProfileDto> appUserProfileUpdated = appUserService.updateAppUserProfile(appUserEditProfileDto);
+            if (appUserProfileUpdated.isPresent()) {
+                return "redirect:/settings/profile";
+            } else {
+                return null;
+            }
         }
     }
 }
