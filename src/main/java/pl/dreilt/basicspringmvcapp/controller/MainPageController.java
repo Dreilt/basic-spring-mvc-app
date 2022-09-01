4b5604341442b9a5698846f4787b00e17fd51ea4
Dreilt <patrykdreilt@gmail.com>
2022-09-01 22:42:45 +0200

    Quick fix 18

--- src/main/java/pl/dreilt/basicspringmvcapp/controller/MainPageController.java
+++ src/main/java/pl/dreilt/basicspringmvcapp/controller/MainPageController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomePageController {
+public class MainPageController {
 
     @GetMapping("/")
-    public String getHomePage() {
+    public String getMainPage() {
         return "index";
     }
 }
