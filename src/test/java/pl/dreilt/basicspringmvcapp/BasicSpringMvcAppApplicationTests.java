df727716ed3788b10113b4de55c5726426c7977d
Dreilt <patrykdreilt@gmail.com>
2022-09-11 15:26:21 +0200

    Quick fix 23

--- src/test/java/pl/dreilt/basicspringmvcapp/BasicSpringMvcAppApplicationTests.java
+++ src/test/java/pl/dreilt/basicspringmvcapp/BasicSpringMvcAppApplicationTests.java
@@ -2,14 +2,12 @@ package pl.dreilt.basicspringmvcapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
 
 @SpringBootTest
 class BasicSpringMvcAppApplicationTests {
 
     @Test
     void contextLoads() {
-        Assert.isTrue(true, "Smoke test runs");
     }
 
 }
