d4dc2cb14bbde118187f6656a347d45ce01fa121
Dreilt <patrykdreilt@gmail.com>
2022-12-30 22:59:43 +0100

    Quick fix

--- src/test/java/pl/dreilt/basicspringmvcapp/service/EventServiceTest.java
+++ src/test/java/pl/dreilt/basicspringmvcapp/service/EventServiceTest.java
@@ -1,5 +1,6 @@
 package pl.dreilt.basicspringmvcapp.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -13,6 +14,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import pl.dreilt.basicspringmvcapp.dto.CityDto;
 import pl.dreilt.basicspringmvcapp.dto.EventBoxDto;
 import pl.dreilt.basicspringmvcapp.dto.EventDto;
+import pl.dreilt.basicspringmvcapp.dto.NewEventDto;
 import pl.dreilt.basicspringmvcapp.entity.AppUser;
 import pl.dreilt.basicspringmvcapp.entity.Event;
 import pl.dreilt.basicspringmvcapp.exception.AppUserNotFoundException;
@@ -30,10 +32,12 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
-import static pl.dreilt.basicspringmvcapp.core.AppUserHelper.createAppUser;
-import static pl.dreilt.basicspringmvcapp.core.AppUserHelper.createUserRole;
+import static pl.dreilt.basicspringmvcapp.core.AppUserHelper.*;
 import static pl.dreilt.basicspringmvcapp.service.AppUserServiceTestHelper.createAppUserDetails;
 import static pl.dreilt.basicspringmvcapp.service.EventServiceTestHelper.*;
 
@@ -49,27 +53,27 @@ class EventServiceTest {
     @InjectMocks
     private EventService eventService;
 
-//    @Test
-//    void shouldCreatedNewEvent() {
-//        // given
-//        Authentication auth = new UsernamePasswordAuthenticationToken(createAppUserDetails("Jan", "Kowalski", "jankowalski@example.com", "ORGANIZER"), null);
-//        SecurityContextHolder.getContext().setAuthentication(auth);
-//        AppUser user = createAppUser(1L, "Jan", "Kowalski", "jankowalski@example.com", createOrganizerRole());
-//        when(appUserRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
-//        NewEventDto newEvent = createNewEventDto();
-//        // when
-//        EventDto eventCreated = eventService.createEvent(newEvent);
-//        // then
-//        Mockito.verify(eventRepository, Mockito.times(1)).save(argThat((Event saved) -> {
-//            Assertions.assertAll("Testing created event",
-//                    () -> assertNull(saved.getId()),
-//                    () -> assertEquals(newEvent.getName(), saved.getName()),
-//                    () -> assertEquals(newEvent.getEventType(), saved.getEventType()),
-//                    () -> assertEquals(newEvent.getDateAndTime(), saved.getDateAndTime())
-//            );
-//            return true;
-//        }));
-//    }
+    @Test
+    void shouldCreatedNewEvent() {
+        // given
+        Authentication auth = new UsernamePasswordAuthenticationToken(createAppUserDetails("Jan", "Kowalski", "jankowalski@example.com", "ORGANIZER"), null);
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        AppUser user = createAppUser(1L, "Jan", "Kowalski", "jankowalski@example.com", createOrganizerRole());
+        when(appUserRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+        NewEventDto newEvent = createNewEventDto();
+        // when
+        EventDto eventCreated = eventService.createEvent(newEvent);
+        // then
+        Mockito.verify(eventRepository, Mockito.times(1)).save(argThat((Event saved) -> {
+            Assertions.assertAll("Testing created event",
+                    () -> assertNull(saved.getId()),
+                    () -> assertEquals(newEvent.getName(), saved.getName()),
+                    () -> assertEquals(newEvent.getEventType(), saved.getEventType()),
+                    () -> assertEquals(newEvent.getDateAndTime(), saved.getDateAndTime())
+            );
+            return true;
+        }));
+    }
 
     @Test
     void shouldGetOneEvent() {
